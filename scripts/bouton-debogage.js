508fcb8e48208de41531393e5d9de897b3ecac6c
mart-tremblay <mart.tremblay.mt5@gmail.com>
2022-04-19 18:46:14 -0400

    - Édition du script qui ajoute une action au clic sur le bouton qui affiche un message (type "ERREUR") au niveau de la console.

--- scripts/bouton-debogage.js
+++ scripts/bouton-debogage.js
@@ -1,4 +1,5 @@
 document.querySelector('#btnDebug').addEventListener('click', () => {
     console.log('Message : Bonjour la police!');
     console.log('Attention : La police, bonjour!');
+    console.log('ERREUR : La police est introuvable!');
 });
